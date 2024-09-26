f95eeb709f46b2b717647ad1ba2b22db2b0f3e5d
Farhad Afridi <afridifarhad149@gmail.com>
2024-09-26 14:30:45 +0500

    add cors library and create a new user an array

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
-const app = express()
+
 const PORT = 3000
+const app = express()
 
+app.use(cors())
+app.use(express.json());
 
 
 const users = [
@@ -15,6 +19,19 @@ const users = [
 
 app.get('/api/users', (req, res) => {
     res.status(200).json({ message: 'Fetched all users!', data: users })
+})
+
+// create new user
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+
+    const newUser = {
+        id: users.length + 1,
+        ...body
+    }
+    
+    users.push(newUser)
+    res.status(201).json({ message: 'New user created!', data: newUser })
 })
 
 app.listen(PORT, () => {

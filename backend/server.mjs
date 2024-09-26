410cd144e3d9d9bf97eedc510ce26301ed94c18c
Farhad Afridi <afridifarhad149@gmail.com>
2024-09-26 14:20:02 +0500

    create an array and added a users

--- backend/server.mjs
+++ backend/server.mjs
@@ -4,7 +4,20 @@ const app = express()
 const PORT = 3000
 
 
+
+const users = [
+    { id: 1, name: 'ali khan', email: 'alikhan@example.com' },
+    { id: 2, name: 'jan ali', email: 'janali@example.com' },
+    {id: 3, name: 'musa alam', email: 'musaalam@example.com'}
+]
+
+
+
+app.get('/api/users', (req, res) => {
+    res.status(200).json({ message: 'Fetched all users!', data: users })
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running here http://localhost:${PORT}`)
-    
+
 })

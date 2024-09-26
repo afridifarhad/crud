334365b2bc469811907e77b48c808ce158b97759
Farhad Afridi <afridifarhad149@gmail.com>
2024-09-26 14:45:02 +0500

    add external library axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const API_URL = 'http://localhost:3000/api/users'
+
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [newUser, setNewUser] = useState('')
+  const [updateUser, setUpdateUser] = useState({id: '', name: ''})
+
+  async function fetchUsers() {
+    const response = await axios.get (API_URL)
+    const content = response.data
+    //console.log(content)
+    setUsers(content.data)
+    
+  }
+
+  useEffect(() =>{
+    fetchUsers()
+
+  }, [])
+ 
+
+
   return (
     <div>App</div>
   )
